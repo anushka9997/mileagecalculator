6048ec08bb91f4ce647e47ac0569f493036465e0
anushka9997 <shrivastavaanushka99@gmail.com>
2019-08-06 11:18:13 -0700

    [CC 1075]|Added comments in the login page

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 console.log("this is my javascript file");
+
+console.log("added the comments section using textarea and specifying the rows and columns in it. ")
+alert("Hope you enjoyed git!!!!");
